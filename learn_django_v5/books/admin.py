a5019384126c2280d3706a8501eaae0cfb358b41
jonndoe <lukichevvasily@gmail.com>
2023-12-13 14:08:04 +0300

    added reviews to books app

--- learn_django_v5/books/admin.py
+++ learn_django_v5/books/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
 
 # Register your models here.
 
 
+class ReviewInLine(admin.TabularInline):
+    model = Review
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInLine,
+    ]
     list_display = (
         "title",
         "author",
