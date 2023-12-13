3e9e421ba554768f36de6b0d032eaa57a9042e41
jonndoe <lukichevvasily@gmail.com>
2023-12-13 19:25:00 +0300

    added search option for books app

--- learn_django_v5/books/urls.py
+++ learn_django_v5/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookDetailView, BookListView
+from .views import BookDetailView, BookListView, SearchResultsView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
     path("<uuid:pk>", BookDetailView.as_view(), name="book_detail"),
+    path("search/", SearchResultsView.as_view(), name="search_results"),
 ]
--- learn_django_v5/books/views.py
+++ learn_django_v5/books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
 from .models import Book
@@ -15,3 +16,13 @@ class BookDetailView(DetailView):
     context_object_name = "book"
     template_name = "books/book_detail.html"
     login_url = "account_login"
+
+
+class SearchResultsView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(Q(title__icontains=query) | Q(title__icontains=query))
